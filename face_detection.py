df0595d1925b544586a8b46b86e1b503a083d313
kvswim <kvswim@gmail.com>
2017-12-02 22:39:25 -0500

    refactor detection prototype

--- face_detection.py
+++ face_detection.py
@@ -13,6 +13,26 @@ import numpy as np
 #init memory stream so images don't need to be saved/read
 #so we aren't slamming the SD
 stream = io.BytesIO()
+
+def getImage():
+	with picamera.PiCamera() as camera:
+		camera.resolution = (640,480)
+		camera.capture(stream, format='jpeg')
+	buff = np.fromstring(stream.getvalue(), dtype = np.uint8)
+	image = cv2.imdecode(buff, 1)
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	return gray
+
+def detectFace(gray):
+	faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+	return faces
+
+def identifyFace(faces):
+	for (x,y,w,h) in faces:
+		cv2.rectangle(image, (x,y), (x+w, y+h), (255,255,0),2)
+		predicted, confidence = predictor.predict(gray[y:y+h, x:x+w])
+	return predicted, confidence
+
 #loading the DB into RAM takes a long time because I'm using a potato for SD memory
 print("Loading database into RAM, this might take a while...")
 predictor = cv2.face.LBPHFaceRecognizer_create()
@@ -20,28 +40,19 @@ predictor.read('k_j_facedetect.xml')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 print("Database loaded. Starting analysis...")
 
-#get image
-with picamera.PiCamera() as camera:
-	camera.resolution = (640,480)
-	camera.capture(stream, format='jpeg')
 
-buff = np.fromstring(stream.getvalue(), dtype = np.uint8)
+grayimg = getImage()
+faces = detectFace(grayimg)
+predicted, confidence = identifyFace(faces)
+print("Prediction: ", predicted)
+print("Confidence: ", confidence)
 
-image = cv2.imdecode(buff, 1)
-
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-
-print('found '+str(len(faces))+' faces')
-for (x,y,w,h) in faces:
-	cv2.rectangle(image, (x,y), (x+w, y+h), (255,255,0),2)
-	predicted, confidence = predictor.predict(gray[y:y+h, x:x+w])
-	print("Prediction:")
-	print(predicted)
-	print("Confidence:")
-	print(confidence)
-	cv2.imshow("Recognized face", image[y:y+h,x:x+w])
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-cv2.imwrite('result.jpg',image)
+# print('found '+str(len(faces))+' faces')
+# print("Prediction:")
+# print(predicted)
+# print("Confidence:")
+# print(confidence)
+# cv2.imshow("Recognized face", image[y:y+h,x:x+w])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite('result.jpg',image)
